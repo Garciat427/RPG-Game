a1925e923a32ee6c73759474e36e759e28071eb1
Troy Garcia <tg1212@outlook.com>
2019-06-18 17:00:51 -0400

    FINALLY fixed spacing and position of ingame characters

--- assets/script.js
+++ assets/script.js
@@ -2,50 +2,97 @@ $(document).ready(function() {
     
     var gameObj = {
 
-        charChosen : null, //Character chosen by user
+        charChosen : null,  //Character chosen by user
+        charHP : null,      //Character HP
+        enmyChosen : null,  //Current Enmy
+        enmyHP: null,       //EnmyHP
+
+        //Character Objs
+        pikachu : {
+            hp : 120,
+            attackInt : 8,
+            dmg : null
+        },
+        magikarp : {
+            hp : 180,
+            attackInt : 8,
+            dmg : null
+        },
+        ampharos : {
+            hp : 150,
+            attackInt : 8,
+            dmg : null
+        },
+        wooloo : {
+            hp : 100,
+            attackInt : 8,
+            dmg : null
+        },
+
 
         onCharSel : function(characterID){
             charClicked = $(characterID).attr("data-letter");
+            
             if (charClicked === "A"){
                 this.charChosen = "Pikachu";
-                $("#charSel").attr("src","assets/Images/pikachu-Transparent.png");
+                $("#charSel1").attr("src","assets/Images/pikachu-Transparent.png");
+                $("#charSel2").attr("src","assets/Images/pikachu-Transparent.png");
+
             }
             else if (charClicked === "B"){
                 this.charChosen = "Magikarp";
-                $("#charSel").attr("src","assets/Images/Magikarp-Transparent.png");
+                $("#charSel1").attr("src","assets/Images/Magikarp-Transparent.png");
+                $("#charSel2").attr("src","assets/Images/Magikarp-Transparent.png");
             }
             else if (charClicked === "C"){
                 this.charChosen = "Ampharos";
-                $("#charSel").attr("src","assets/Images/Ampharos-Transparent.png");
+                $("#charSel1").attr("src","assets/Images/Ampharos-Transparent.png");
+                $("#charSel2").attr("src","assets/Images/Ampharos-Transparent.png");
             }
             else{
                 this.charChosen = "Wooloo";
-                $("#charSel").attr("src","assets/Images/Wooloo-Transparent.png");
+                $("#charSel1").attr("src","assets/Images/Wooloo-Transparent.png");
+                $("#charSel2").attr("src","assets/Images/Wooloo-Transparent.png");
             }
             $("#startBtn").removeClass("invisible");
             $("#chosenLbl").removeClass("invisible");
         },
 
         onEnmySel : function(characterID){
-            charClicked = $(characterID).attr("data-letter");
-            if (charClicked === "A"){
-                this.charChosen = "Pikachu";
+            var enmyClicked = $(characterID).attr("data-letter");
+            
+
+            if (enmyClicked === "A"){
+                this.enmyChosen = "Pikachu";
                 $("#enmySel").attr("src","assets/Images/pikachu-Transparent.png");
             }
-            else if (charClicked === "B"){
-                this.charChosen = "Magikarp";
+            else if (enmyClicked === "B"){
+                this.enmyChosen = "Magikarp";
                 $("#enmySel").attr("src","assets/Images/Magikarp-Transparent.png");
             }
-            else if (charClicked === "C"){
-                this.charChosen = "Ampharos";
+            else if (enmyClicked === "C"){
+                this.enmyChosen = "Ampharos";
                 $("#enmySel").attr("src","assets/Images/Ampharos-Transparent.png");
             }
             else{
-                this.charChosen = "Wooloo";
+                this.enmyChosen = "Wooloo";
                 $("#enmySel").attr("src","assets/Images/Wooloo-Transparent.png");
             }
         },
         onStart : function() {
+            console.log(this.charChosen)
+            if (this.charChosen === "Pikachu"){
+                $("#enmyA").remove();
+            }
+            else if (this.charChosen === "Magikarp"){
+                $("#enmyB").remove();
+            }
+            else if (this.charChosen === "Ampharos"){
+                $("#enmyC").remove();
+            }
+            else{
+                $("#enmyD").remove();
+            }
 
         }
     }
@@ -64,5 +111,8 @@ $(document).ready(function() {
     });
     $(".enmyImg").on("click", function(){
         gameObj.onEnmySel(this);
+    });
+    $("#startBtn").on("click", function(){
+        gameObj.onStart();
     });
 });
