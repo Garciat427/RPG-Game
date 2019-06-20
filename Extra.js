3bb15a61edbda9c5e03a0722990abda4f1fdf408
Troy Garcia <tg1212@outlook.com>
2019-06-20 00:42:44 -0400

    Finished Animation and design of game

---
+++ Extra.js
@@ -0,0 +1,11 @@
+console.log("");
+            console.log("-----------------Log----------------------");
+            console.log("Current Player Dmg: " + this.playerChosen.dmg);
+            console.log("Current Player Starting HP: " + this.playerChosen.hp);
+            console.log("Current Player HP: " + this.playerHP);
+            console.log("-----------------");
+            console.log("Current Enemy Dmg: " + this.enemyChosen.dmg);
+            console.log("Current Enemy Starting HP: " + this.enemyChosen.hp);
+            console.log("Current Enemy HP: " + this.enemyHP);
+            console.log("-----------------");
+            console.log("Player attacked");s
--- assets/script.js
+++ assets/script.js
@@ -1,118 +1,211 @@
 $(document).ready(function() {
-    
     var gameObj = {
 
-        charChosen : null,  //Character chosen by user
-        charHP : null,      //Character HP
-        enmyChosen : null,  //Current Enmy
-        enmyHP: null,       //EnmyHP
+        playerChosen : null,            //Character chosen by user
+        playerHP : null,                //Character HP
+        playerHPBar : $("#playerHP"),   //Character HP Bar
+        enemyChosen : null,              //Current Enemy
+        enemyHP: null,                   //Enemy HP
+        enemyHPBar : $("#enemyHP"),       //Enemy HP Bar
 
         //Character Objs
         pikachu : {
             hp : 120,
             attackInt : 8,
-            dmg : null
+            dmg : 8,
         },
         magikarp : {
             hp : 180,
             attackInt : 8,
-            dmg : null
+            dmg : 5,
         },
         ampharos : {
             hp : 150,
             attackInt : 8,
-            dmg : null
+            dmg : null,
         },
         wooloo : {
             hp : 100,
             attackInt : 8,
-            dmg : null
+            dmg : null,
         },
 
 
         onCharSel : function(characterID){
             charClicked = $(characterID).attr("data-letter");
+            $("#charSel1").css({ opacity : 0 });
             
             if (charClicked === "A"){
-                this.charChosen = "Pikachu";
-                $("#charSel1").attr("src","assets/Images/pikachu-Transparent.png");
-                $("#charSel2").attr("src","assets/Images/pikachu-Transparent.png");
-
+                this.playerChosen = this.pikachu; //Set Char
+                //Set Obj Images
+                $("#charSel1").attr("src","assets/Images/Characters/pikachu-Transparent.png");
+                $("#charSel2").attr("src","assets/Images/Characters/pikachu-Transparent.png");
             }
             else if (charClicked === "B"){
-                this.charChosen = "Magikarp";
-                $("#charSel1").attr("src","assets/Images/Magikarp-Transparent.png");
-                $("#charSel2").attr("src","assets/Images/Magikarp-Transparent.png");
+                this.playerChosen = this.magikarp;//Set Char
+                //Set Obj Images
+                $("#charSel1").attr("src","assets/Images/Characters/Magikarp-Transparent.png");
+                $("#charSel2").attr("src","assets/Images/Characters/Magikarp-Transparent.png");
             }
             else if (charClicked === "C"){
-                this.charChosen = "Ampharos";
-                $("#charSel1").attr("src","assets/Images/Ampharos-Transparent.png");
-                $("#charSel2").attr("src","assets/Images/Ampharos-Transparent.png");
+                this.playerChosen = this.ampharos;//Set Char
+                //Set Obj Images
+                $("#charSel1").attr("src","assets/Images/Characters/Ampharos-Transparent.png");
+                $("#charSel2").attr("src","assets/Images/Characters/Ampharos-Transparent.png");
             }
             else{
-                this.charChosen = "Wooloo";
-                $("#charSel1").attr("src","assets/Images/Wooloo-Transparent.png");
-                $("#charSel2").attr("src","assets/Images/Wooloo-Transparent.png");
+                this.playerChosen = this.wooloo;//Set Char
+                //Set Obj Images
+                $("#charSel1").attr("src","assets/Images/Characters/Wooloo-Transparent.png");
+                $("#charSel2").attr("src","assets/Images/Characters/Wooloo-Transparent.png");
             }
-            $("#startBtn").removeClass("invisible");
-            $("#chosenLbl").removeClass("invisible");
+
+            $("#charSel1").animate({ opacity: "1" });
+            $("#startBtn").animate({ opacity: "1" });
+            $("#chosenLbl").animate({ opacity: "1" });;
+            this.playerHP = this.playerChosen.hp;
         },
 
-        onEnmySel : function(characterID){
-            var enmyClicked = $(characterID).attr("data-letter");
-            
-
-            if (enmyClicked === "A"){
-                this.enmyChosen = "Pikachu";
-                $("#enmySel").attr("src","assets/Images/pikachu-Transparent.png");
-            }
-            else if (enmyClicked === "B"){
-                this.enmyChosen = "Magikarp";
-                $("#enmySel").attr("src","assets/Images/Magikarp-Transparent.png");
-            }
-            else if (enmyClicked === "C"){
-                this.enmyChosen = "Ampharos";
-                $("#enmySel").attr("src","assets/Images/Ampharos-Transparent.png");
-            }
-            else{
-                this.enmyChosen = "Wooloo";
-                $("#enmySel").attr("src","assets/Images/Wooloo-Transparent.png");
-            }
-        },
         onStart : function() {
-            console.log(this.charChosen)
-            if (this.charChosen === "Pikachu"){
-                $("#enmyA").remove();
+            $("#selectionCard").empty(); //Remove Char Selection Card
+            $("#battlefieldCard").removeClass("invisible"); //Make Battlefield Card Visible
+            if (this.playerChosen === this.pikachu){
+                $("#enemyA").remove();
             }
-            else if (this.charChosen === "Magikarp"){
-                $("#enmyB").remove();
+            else if (this.playerChosen === this.magikarp){
+                $("#enemyB").remove();
             }
-            else if (this.charChosen === "Ampharos"){
-                $("#enmyC").remove();
+            else if (this.playerChosen === this.ampharos){
+                $("#enemyC").remove();
             }
             else{
-                $("#enmyD").remove();
+                $("#enemyD").remove();
             }
+            this.playerHPBar.attr("style", "width: 100%");
+        },
 
-        }
+        onenemySel : function(characterID){
+            var enemyClicked = $(characterID).attr("data-letter");
+            if (enemyClicked !== "F"){
+                
+
+                if (enemyClicked === "A"){
+                    this.enemyChosen = this.pikachu;
+                    $("#enemySel").attr("src","assets/Images/Characters/pikachu-Transparent.png");
+                }
+                else if (enemyClicked === "B"){
+                    this.enemyChosen = this.magikarp;
+                    $("#enemySel").attr("src","assets/Images/Characters/Magikarp-Transparent.png");
+                }
+                else if (enemyClicked === "C"){
+                    this.enemyChosen = this.ampharos;
+                    $("#enemySel").attr("src","assets/Images/Characters/Ampharos-Transparent.png");
+                }
+                else{
+                    this.enemyChosen = this.wooloo;
+                    $("#enemySel").attr("src","assets/Images/Characters/Wooloo-Transparent.png");
+                }
+
+                $("#enemySelection").animate({ opacity: "0" }); //Make enemySel Invisible
+                $("#enemySel").animate({ opacity: "1" });       //Make Enemy Visible
+                $("#charSel2").animate({ opacity: "1" });       //Make Player Visible
+                $("#gameStatusCard").animate({ opacity: "1" }); //Make Game status Visible
+                $("#status").text("Press Attack to fight!");    //Change Game status Text
+
+                this.enemyHP = this.enemyChosen.hp;
+            }
+        },
+
+        runUpdate : function() {
+            var percentHP;
+            console.log("");
+            console.log("-----------------Log----------------------");
+            console.log("Current Player Dmg: " + this.playerChosen.dmg);
+            console.log("Current Player Starting HP: " + this.playerChosen.hp);
+            console.log("Current Player HP: " + this.playerHP);
+            console.log("-----------------");
+            console.log("Current Enemy Dmg: " + this.enemyChosen.dmg);
+            console.log("Current Enemy Starting HP: " + this.enemyChosen.hp);
+            console.log("Current Enemy HP: " + this.enemyHP);
+            console.log("-----------------");
+            
+            console.log("Player attacked");
+            this.enemyHP -= this.playerChosen.dmg; //Player Attacks
+            console.log("Current Enemy HP: " + this.enemyHP);
+
+            if (this.enemyHP <= 0){     //If enemy hp <= 0 (Enemy Lost)
+                
+                $("#enemySelection").animate({ opacity: "1" });     //Show Enemy Select
+                $("#status").text("Choose Your next enemy!");       //Change Game status Text
+                $("#gameStatusCard").animate({ opacity: "0" });     //Hide Game Status
+                $("#charSel2").animate({ opacity: "0" });           //Hide Character
+                $("#enemySel").animate({ opacity: "0" });          //Hide Enemy
+
+                if (this.enemyChosen === this.pikachu){
+                    console.log("pikachu");
+                    $("#enemyAimg").attr("src","assets/Images/Characters/pikachu-Transparent-X.png").attr("data-letter","F")
+                }
+                else if (this.enemyChosen === this.magikarp){
+                    console.log("magikarp");
+                    $("#enemyBimg").attr("src","assets/Images/Characters/Magikarp-Transparent-X.png").attr("data-letter","F")
+                }
+                else if (this.enemyChosen === this.ampharos){
+                    console.log("ampharos");
+                    $("#enemyCimg").attr("src","assets/Images/Characters/Ampharos-Transparent-X.png").attr("data-letter","F")
+                }
+                else{
+                    console.log("pikachu");
+                    $("#enemyDimg").attr("src","assets/Images/Characters/Wooloo-Transparent-X.png").attr("data-letter","F")
+                }
+
+                
+                
+                this.progressUpdate(this.enemyHPBar,0)
+            }
+            else{                       //Else Enemy Proceeds With attack
+                console.log("Enemy attacked");
+                this.playerHP -= this.enemyChosen.dmg;
+                console.log("Current Player HP: " + this.playerHP);
+
+                if (this.playerHP <= 0){ //If Player hp <= 0 (Player Lost)
+                    console.log("Player Lost");
+                    this.progressUpdate(this.playerHPBar,0)
+                }
+                else{
+                    console.log("Regular Run");
+                    //Calculate and update Enemy HP
+                    percentHP = ((this.enemyHP / this.enemyChosen.hp) * 100); //Calculate Percentage
+                    this.progressUpdate(this.enemyHPBar,percentHP);
+                    
+                    //Calculate and update Player HP
+                    percentHP = ((this.playerHP / this.playerChosen.hp) * 100); //Calculate Percentage
+                    this.progressUpdate(this.playerHPBar,percentHP);
+                }
+            }
+            //Increase Player Dmg
+            this.playerChosen.dmg += this.playerChosen.attackInt;
+        },
+        progressUpdate : function(selCharBar,percentHP) {
+            if (percentHP === 0){ //If dead then Reset bar
+                percentHP = 100
+            }   
+            selCharBar.attr("style", "width: " + percentHP + "%");
+        },
     }
     
     
     
-    //Listners
-    $( ".charImg" ).hover(function () {
-        $(this).css("background","green");
-    },
-    function () {
-        $(this).css("background","");
-    });
+    //Listeners
     $(".charImg").on("click", function(){
         gameObj.onCharSel(this);
     });
-    $(".enmyImg").on("click", function(){
-        gameObj.onEnmySel(this);
+    $(".enemyImg").on("click", function(){
+        gameObj.onenemySel(this);
     });
     $("#startBtn").on("click", function(){
         gameObj.onStart();
+    });
+    $("#atkBtn").on("click", function(){
+        gameObj.runUpdate();
     });
 });
